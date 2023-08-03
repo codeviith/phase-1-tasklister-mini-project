d7c3af115406f44f8a3d84b7c7db0f428b3d2a4e
codeviith <codeviith@Cinya>
2023-08-03 09:56:15 -0400

    Mini Tasklister Project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const newTaskInput = document.getElementById("new-task-description").value;
+    // console.log(newTaskInput)
+    // const list = 
+    const taskLi = document.createElement("li");
+
+    taskLi.textContent = newTaskInput;
+    // console.log(taskLi)
+    addTasksToList(taskLi);
+    
+    form.reset();
+
+    const button = document.createElement("button");
+    button.textContent = "X";
+
+    taskLi.appendChild(button);
+
+    taskLi.addEventListener("click", () => {
+      taskLi.remove();
+    })
+  })
 });
+
+function addTasksToList(task) {
+  // console.log(task)
+  const toDoList = document.getElementById("tasks");
+  // console.log(toDoList)
+
+  toDoList.appendChild(task);
+}
+
+
+
+
+
+
+
+
+
+// const newTaskInput = document.getElementById("new-task-description").value;
+// const newTasks = document.getElementById("tasks");
+
+
+
+
+
+
+
+
+
